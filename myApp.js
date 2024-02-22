3d0b1a8bb7cb496e1a5550bea3b3d9bee3dfa6a5
Youssef Hajji <hajji.youssef.personal@gmail.com>
2024-02-22 11:30:53 +0100

    changes made

--- myApp.js
+++ myApp.js
@@ -1,10 +1,35 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 console.log("Hello World");
-app.get("/", (req, res) => {
-    res.send("Hello Express");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
+app.use("/public",express.static(__dirname +'/public'))
+app.use(function(req , res, next){
+    console.log(`${req.method} / ${req.path} - ${req.ip}`)
+    next()
+})
+app.get("/json", (req,res)=>{
+    if(process.env.MESSAGE_STYLE == 'uppercase'){
+        res.json({"message":"HELLO JSON"})    
+    }else{
+        res.json({"message":"Hello json"})
+    }
+})
 
+app.get('/now', function(req,res,next){
+    req.time=newDate().toString();
+    next();
+},
+    (req, res) =>{
+        res.send(
+            {
+                time: req.time
+            }
+        )
+    }
+)
 
 
 
